d60e3ba51f3caf4437cdcc5a7311bb83a352ab21
Shubham Katakwar <shubham.katakwar.7@gmail.com>
2024-04-23 06:30:58 +0530

    C3:Button

--- qtify/src/components/FeedbackButton/FeedbackButton.jsx
+++ qtify/src/components/FeedbackButton/FeedbackButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import styles from "./FeedbackButton.module.css";
+import { Typography } from "@mui/material";
 
 function FeedbackButton() {
   return (
-  <></>
-  )
+    <Button
+      variant="contained"
+      color="inherit"
+      className={styles.feedbackButton}
+    >
+      <Typography className={styles.buttonName}>Give Feedback</Typography>
+    </Button>
+  );
 }
 
-export default FeedbackButton
+export default FeedbackButton;
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Link } from "@mui/material";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
 import styles from "./Navbar.module.css"
 import FeedbackButton from "../FeedbackButton/FeedbackButton";
 import Logo from "../Logo/Logo";
@@ -12,17 +10,13 @@ import Logo from "../Logo/Logo";
 function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }} className={styles.navbar}>
-      <AppBar position="static" className="styles.appBar">
-        <Toolbar>
+      <AppBar position="static" className={styles.appbar}>
+        <Toolbar className={styles.toolbar}>
           <Link to="/">
             <Logo />
           </Link>
-          <IconButton color="inherit" aria-label="searchBar">
-            {/* <SearchBar /> */}
-          </IconButton>
-          <Button color="inherit" aria-label="feedbackButton">
-            <FeedbackButton />
-          </Button>
+          {/* <SearchBar /> */}
+          <FeedbackButton />
         </Toolbar>
       </AppBar>
     </Box>
