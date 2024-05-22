0d2c5d1ae35c5dcca3d80ece5930520f120970dc
Shubham Katakwar <shubham.katakwar.7@gmail.com>
2024-05-23 00:09:57 +0530

    tooltip added to qtify albums

--- qtify_with_tooltip/src/components/Cards/Cards.jsx
+++ qtify_with_tooltip/src/components/Cards/Cards.jsx
@@ -8,34 +8,49 @@ import {
   CardActionArea,
   CardActions,
   Chip,
+  Tooltip,
 } from "@mui/material";
 import styles from "./Cards.module.css";
 
 function Cards({ data, tabData }) {
+  // console.log("Card.jsx Cards Component debug data::", data);
   return (
     <Card className={styles.card}>
       <Box className={styles.innerCard}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            image={data.image?data.image:require("../../assets/cardImage.png")}
-            alt="Card Image Not Found"
-            className={styles.img}
-          />
-        </CardActionArea>
-        { !tabData ? (
-        <CardActions className={styles.chipContainer}>
-          <Chip label={`${data.follows?data.follows:100} Follows`} className={styles.chips}/>
-        </CardActions>
+        {!tabData ? (
+          <Tooltip title={`${data.songs.length} songs`} placement="top" arrow>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image={data.image ? data.image : require("../../assets/cardImage.png")}
+                alt="Card Image Not Found"
+                className={styles.img}
+              />
+            </CardActionArea>
+          </Tooltip>
         ) : (
-        <CardActions className={styles.chipContainer}>
-          <Chip label={`${data.likes?data.likes:100} Likes`} className={styles.chips}/>
-        </CardActions>
-        ) }
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              image={data.image ? data.image : require("../../assets/cardImage.png")}
+              alt="Card Image Not Found"
+              className={styles.img}
+            />
+          </CardActionArea>
+        )}
+        {!tabData ? (
+          <CardActions className={styles.chipContainer}>
+            <Chip label={`${data.follows ? data.follows : 100} Follows`} className={styles.chips} />
+          </CardActions>
+        ) : (
+          <CardActions className={styles.chipContainer}>
+            <Chip label={`${data.likes ? data.likes : 100} Likes`} className={styles.chips} />
+          </CardActions>
+        )}
       </Box>
       <CardContent>
         <Typography gutterBottom variant="h6" component="div" className={styles.name}>
-          {data.title?data.title:"New English Songs"}
+          {data.title ? data.title : "New English Songs"}
         </Typography>
       </CardContent>
     </Card>
